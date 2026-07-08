91f709eb790a1b5171d3d7c99c86bd82eb377b6d
Angel Gupta <anglegupta037@gmail.com>
2026-07-08 15:49:22 +0530

    Time: 181 ms (42.51%), Space: 40.7 MB (34.78%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.py
@@ -0,0 +1,41 @@
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        
+        mid = self.middle(head)
+        right = mid.next
+        mid.next = None
+        left = head
+        
+        left = self.sortList(left)
+        right = self.sortList(right)
+        
+        return self.mergeLL(left, right)
+    
+    def middle(self, head):
+        if head is None or head.next is None:
+            return head
+        slow = head
+        fast = head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+    
+    def mergeLL(self, l1, l2):
+        dummy = ListNode(-1)
+        temp = dummy
+        while l1 and l2:
+            if l1.val < l2.val:  
+                temp.next = l1
+                l1 = l1.next
+            else:
+                temp.next = l2
+                l2 = l2.next
+            temp = temp.next
+        if l1:
+            temp.next = l1
+        else:
+            temp.next = l2
+        return dummy.next
