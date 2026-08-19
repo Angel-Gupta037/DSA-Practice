38bd8f2cc0870fa2813e628fd0574280b4e6d5cd
Angel Gupta <anglegupta037@gmail.com>
2026-08-19 23:45:18 +0530

    Time: 7 ms (76.05%), Space: 19.6 MB (33.69%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,22 +1,15 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        ans = []
-        
-        def recurse(ind, t, ds):
-            # Base case
-            if ind == len(candidates):
-                if t == 0:
-                    ans.append(ds[:])  
+        res=[]
+        def helper(i,ds,target):
+            if target==0:
+                res.append(ds[:])
                 return
-            
-            # DON'T 
-            recurse(ind + 1, t, ds)
-            
-            #PICK (if it fits)
-            if candidates[ind] <= t:  
-                ds.append(candidates[ind]) 
-                recurse(ind, t - candidates[ind], ds)
-                ds.pop() 
-        
-        recurse(0, target, [])  #Call the function
-        return ans
+            if i==len(candidates) or target<0:
+                return
+            ds.append(candidates[i])
+            helper(i,ds,target-candidates[i])
+            ds.pop()
+            helper(i+1,ds,target)
+        helper(0,[],target)
+        return res
