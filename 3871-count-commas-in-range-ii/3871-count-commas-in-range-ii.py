ee3140b8a5fbd020ebc1f457ea6c2264c46cb7ac
Angel Gupta <anglegupta037@gmail.com>
2026-09-09 20:58:27 +0530

    Time: 0 ms (100%), Space: 19.4 MB (13.61%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countCommas(self, n: int) -> int:
+        total = 0
+        
+        # Power of 1000: 1000, 1000000, 1000000000, ...
+        power = 1000
+        commas = 1
+        
+        while power <= n:
+            # Count numbers from 'power' to min(n, power*1000 - 1)
+            end = min(n, power * 1000 - 1)
+            total += (end - power + 1) * commas
+            
+            # Move to next range
+            power *= 1000
+            commas += 1
+        
+        return total 
+        
